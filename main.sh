7a2397857b5d8f32fd63fad6f7534b8e9ccd237c
Meysam Azad <meysam@developer-friendly.blog>
2024-10-19 14:43:49 +0700

    fix(CI): remove v from version numbers

--- main.sh
+++ main.sh
@@ -10,15 +10,15 @@ else
     ARCH="amd64"
 fi
 
-wget https://github.com/gruntwork-io/terragrunt/releases/download/${TG_VERSION}/terragrunt_linux_${ARCH} -O "${TG_FILE}"
-wget "https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/${TAC_VERSION}/terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}"
+wget https://github.com/gruntwork-io/terragrunt/releases/download/v${TG_VERSION}/terragrunt_linux_${ARCH} -O "${TG_FILE}"
+wget "https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/v${TAC_VERSION}/terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}"
 
 if [ -z "${TG_SHA256_SUM}"]; then
-    TG_SHA256_SUM=$(wget -qO- https://github.com/gruntwork-io/terragrunt/releases/download/${TG_VERSION}/SHA256SUMS | grep "terragrunt_linux_${ARCH}" | awk '{print $1}')
+    TG_SHA256_SUM=$(wget -qO- https://github.com/gruntwork-io/terragrunt/releases/download/v${TG_VERSION}/SHA256SUMS | grep "terragrunt_linux_${ARCH}" | awk '{print $1}')
 fi
 
 if [ -z "${TAC_SHA256_SUM}"]; then
-    TAC_SHA256_SUM=$(wget -qO- https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/${TAC_VERSION}/SHA256SUMS | grep "terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}" | awk '{print $1}')
+    TAC_SHA256_SUM=$(wget -qO- https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/v${TAC_VERSION}/SHA256SUMS | grep "terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}" | awk '{print $1}')
 fi
 
 echo "${TG_SHA256_SUM} ${TG_FILE}_linux_${ARCH}" | sha256sum -c
