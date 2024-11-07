3c99ffb3fc01930b8a68ec2e2739a240d34a51e1
Meysam Azad <meysam@developer-friendly.blog>
2024-11-07 14:28:14 +0700

    chore(docker): download tofu as well

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,33 @@
+
+FROM curlimages/curl AS bins
+
 ARG ATLANTIS_VERSION="v0.30.0"
-
-FROM ghcr.io/runatlantis/atlantis:${ATLANTIS_VERSION}
-
 ARG TG_VERSION="0.68.4"
 ARG TAC_VERSION="1.19.0"
-
-LABEL maintainer="Meysam Azad <meysam@developer-friendly.blog>"
-LABEL terragrunt_version=${TG_VERSION}
-LABEL terragrunt_atlantis_config_version=${TAC_VERSION}
+ARG TOFU_VERSION="1.8.5"
 
 ARG TG_SHA256_SUM=""
 ARG TAC_SHA256_SUM=""
+ARG ATLANTIS_SHA256_SUM=""
+ARG TOFU_SHA256_SUM=""
+
+LABEL maintainer="Meysam Azad <meysam@developer-friendly.blog>"
+LABEL atlantis_version=${ATLANTIS_VERSION}
+LABEL terragrunt_atlantis_config_version=${TAC_VERSION}
+LABEL terragrunt_version=${TG_VERSION}
+
+USER root
 
 COPY main.sh /
 
-USER root
-RUN apk update && \
-    apk upgrade && \
-    rm -rf /var/cache/apk/* && \
-    /main.sh && \
+RUN /main.sh && \
     rm /main.sh
-USER atlantis:atlantis
+
+FROM gcr.io/distroless/base-nossl-debian12:nonroot
+
+COPY --from=bins /usr/local/bin/atlantis /usr/local/bin/atlantis
+COPY --from=bins /usr/local/bin/tofu /usr/local/bin/tofu
+COPY --from=bins /usr/local/bin/terragrunt /usr/local/bin/terragrunt
+COPY --from=bins /usr/local/bin/terragrunt-atlantis-config /usr/local/bin/terragrunt-atlantis-config
+
+CMD ["/usr/local/bin/atlantis", "server"]
--- main.sh
+++ main.sh
@@ -11,8 +11,15 @@ else
     exit 1
 fi
 
+TG_VERSION=${TG_VERSION#v}
+TAC_VERSION=${TAC_VERSION#v}
+ATLANTIS_VERSION=${ATLANTIS_VERSION#v}
+TOFU_VERSION=${TOFU_VERSION#v}
+
 wget https://github.com/gruntwork-io/terragrunt/releases/download/v${TG_VERSION}/terragrunt_linux_${ARCH}
-wget "https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/v${TAC_VERSION}/terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}"
+wget https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/v${TAC_VERSION}/terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}
+wget https://github.com/runatlantis/atlantis/releases/download/v${ATLANTIS_VERSION}/atlantis_linux_${ARCH}.zip
+wget https://github.com/opentofu/opentofu/releases/download/v${TOFU_VERSION}/tofu_${TOFU_VERSION}_linux_${ARCH}.tar.gz
 
 if [ -z "${TG_SHA256_SUM}"]; then
     TG_SHA256_SUM=$(wget -qO- https://github.com/gruntwork-io/terragrunt/releases/download/v${TG_VERSION}/SHA256SUMS | grep "terragrunt_linux_${ARCH}" | awk '{print $1}')
@@ -20,6 +27,14 @@ fi
 
 if [ -z "${TAC_SHA256_SUM}"]; then
     TAC_SHA256_SUM=$(wget -qO- https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/v${TAC_VERSION}/SHA256SUMS | grep "terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}" | awk '{print $1}')
+fi
+
+if [ -z "${ATLANTIS_SHA256_SUM}"]; then
+    ATLANTIS_SHA256_SUM=$(wget -qO- https://github.com/runatlantis/atlantis/releases/download/v${ATLANTIS_VERSION}/checksums.txt | grep "atlantis_linux_${ARCH}.zip" | awk '{print $1}')
+fi
+
+if [ -z "${TOFU_SHA256_SUM}"]; then
+    TOFU_SHA256_SUM=$(wget -qO- https://github.com/opentofu/opentofu/releases/download/v${TOFU_VERSION}/tofu_${TOFU_VERSION}_SHA256SUMS | grep "tofu_${TOFU_VERSION}_linux_${ARCH}.tar.gz" | awk '{print $1}')
 fi
 
 echo "${TG_SHA256_SUM} terragrunt_linux_${ARCH}" | sha256sum -c
@@ -31,3 +46,15 @@ echo "${TAC_SHA256_SUM}  terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}
 mv "terragrunt-atlantis-config_${TAC_VERSION}_linux_${ARCH}" /usr/local/bin/terragrunt-atlantis-config
 chmod 755 /usr/local/bin/terragrunt-atlantis-config
 terragrunt-atlantis-config version
+
+echo "${ATLANTIS_SHA256_SUM}  atlantis_linux_${ARCH}.zip" | sha256sum -c
+unzip "atlantis_linux_${ARCH}.zip"
+mv atlantis /usr/local/bin/atlantis
+chmod 755 /usr/local/bin/atlantis
+atlantis version
+
+echo "${TOFU_SHA256_SUM}  tofu_${TOFU_VERSION}_linux_${ARCH}.tar.gz" | sha256sum -c
+tar -xvf "tofu_${TOFU_VERSION}_linux_${ARCH}.tar.gz"
+mv tofu /usr/local/bin/tofu
+chmod 755 /usr/local/bin/tofu
+tofu -version
